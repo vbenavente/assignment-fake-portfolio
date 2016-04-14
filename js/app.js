7660167dd111751dd03641c44e4b26a3f89f2c7a
vbenavente <vbenavente@hotmail.com>
2016-04-13 23:53:21 -0700

    added code to add image to project object

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var projects = [];
 function Project (items) {
   this.title = items.title;
   this.category = items.category;
+  this.projectImage = items.projectImage;
   this.gitUrl = items.gitUrl;
   this.publishedOn = items.publishedOn;
   this.body = items.body;
@@ -12,6 +13,7 @@ Project.prototype.toHtml = function() {
   var $newProject = $('article.template').clone();
 
   $newProject.attr('data-category', this.category);
+  $newProject.find('address').attr('img', this.projectImage);
   $newProject.find('a').attr('href', this.gitUrl);
   $newProject.find('h1').html(this.title);
   $newProject.find('article-body').html(this.body);
--- js/projectData.js
+++ js/projectData.js
@@ -2,6 +2,7 @@ var myProjects = [
   {
     title: 'Budget Buddy',
     category: 'form',
+    projectImage: 'images/budget-buddy.png',
     gitUrl: 'http://stefuhnee.github.io/budget-buddy/',
     publishedOn: '2016-04-08',
     body: '<p>My First Project</p>'
